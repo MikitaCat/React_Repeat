c6a0d93ef816eb62dea6835c050a6cf8c67d547f
MikitaCat <nikita.proger21@gmail.com>
2021-12-07 23:36:34 +0300

    Object with input value and counter

--- src/App.js
+++ src/App.js
@@ -13,10 +13,16 @@ function App() {
     setLikes(likes - 1);
   }
 
+  const obj = {
+    value: value,
+    count: likes,
+  };
+
   return (
     <div>
       <h1>Counter {likes}</h1>
       <input value={value} onChange={(event) => setValue(event.target.value)} />
+      <button onClick={() => console.log(obj)}>On console</button>
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button>
     </div>
