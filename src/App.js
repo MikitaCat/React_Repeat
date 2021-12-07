97390b00a257ce9966775b8e4053a81ac9f947e2
MikitaCat <nikita.proger21@gmail.com>
2021-12-07 23:22:20 +0300

    Simple counter, implemented using useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App">Working</div>;
+  const [likes, setLikes] = useState(0);
+  function increment() {
+    setLikes(likes + 1);
+  }
+
+  function decrement() {
+    setLikes(likes - 1);
+  }
+  return (
+    <div>
+      <h1>{likes}</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+    </div>
+  );
 }
 
 export default App;
