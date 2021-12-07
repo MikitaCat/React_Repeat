0a28b9412078b4d8f3b21fee96533cc5559c1fba
MikitaCat <nikita.proger21@gmail.com>
2021-12-08 02:03:02 +0300

    Destructurisation of props in PostItem

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import '../src/styles/App.css';
 import PostItem from './components/PostItem';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'Great Language' },
+    { id: 2, title: 'Python', body: 'BackEnd Language' },
+    { id: 1, title: 'C#', body: 'Language for .NET, Unity' },
+  ]);
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: 'JavaScript', body: 'Discription' }} />
+      {posts.map((post) => {
+        return <PostItem post={post} key={post.id} />;
+      })}
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 function PostItem(props) {
+  const { id, title, body } = props.post;
   return (
     <div className="post">
       <div className="post__content">
         <strong>
-          {props.post.id} {props.post.title}
+          {id} {title}
         </strong>
-        <div>{props.post.body}</div>
+        <div>{body}</div>
       </div>
       <div className="post__btns">
         <button>Delete</button>
