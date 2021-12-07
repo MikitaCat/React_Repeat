008e35a9e0638853b5ebefe299b71be3d508aa39
MikitaCat <nikita.proger21@gmail.com>
2021-12-08 02:27:53 +0300

    Returned an array of posts to the App component and passed it to the PostList using props. Destructured the props in the PostList

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import PostItem from './components/PostItem';
 import PostList from './components/PostList';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'Great Language' },
+    { id: 2, title: 'Python', body: 'BackEnd Language' },
+    { id: 3, title: 'C#', body: 'Language for .NET, Unity' },
+    { id: 4, title: 'C#', body: 'Language for .NET, Unity' },
+  ]);
   return (
     <div className="App">
-      <PostList />
+      <PostList posts={posts} title="Posts List" />
     </div>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,16 +2,10 @@ import React from 'react';
 import PostItem from '../components/PostItem';
 import { useState } from 'react';
 
-function PostList() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'JavaScript', body: 'Great Language' },
-    { id: 2, title: 'Python', body: 'BackEnd Language' },
-    { id: 3, title: 'C#', body: 'Language for .NET, Unity' },
-    { id: 4, title: 'C#', body: 'Language for .NET, Unity' },
-  ]);
+function PostList({ posts, title }) {
   return (
     <div>
-      <h1 style={{ textAlign: 'center' }}>All Posts</h1>
+      <h1 style={{ textAlign: 'center' }}>{title}</h1>
       {posts.map((post) => {
         return <PostItem post={post} key={post.id} />;
       })}
