265ebf9c398c77c24ffdb9b93677e5e108004b65
MikitaCat <nikita.proger21@gmail.com>
2021-12-10 02:35:31 +0300

    Created first animation using React Transition Group

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import PostItem from '../components/PostItem';
 import { useState } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 function PostList({ posts, title, remove }) {
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>{title}</h1>
-      {posts.map((post, index) => {
-        return <PostItem remove={remove} number={index + 1} post={post} key={post.id} />;
-      })}
+      <TransitionGroup>
+        {posts.map((post, index) => {
+          return (
+            <CSSTransition key={post.id} timeout={500} classNames="post">
+              <PostItem remove={remove} number={index + 1} post={post} />
+            </CSSTransition>
+          );
+        })}
+      </TransitionGroup>
     </div>
   );
 }
