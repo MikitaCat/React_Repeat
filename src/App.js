fb2f06d7919e8a634c12b58cfdaa7569f9266293
MikitaCat <nikita.proger21@gmail.com>
2021-12-09 19:50:58 +0300

    started to do sorting. First part: Made the selection as a separate UI component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import '../src/styles/App.css';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
+import MySelect from './components/UI/Select/MySelect';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,6 +12,8 @@ function App() {
     { id: 3, title: 'C#', body: 'Language for .NET, Unity' },
     { id: 4, title: 'C#', body: 'Language for .NET, Unity' },
   ]);
+
+  const [selectedSort, setSelectedSort] = useState('');
 
   // const addNewPost = (event) => {
   //   event.preventDefault();
@@ -26,9 +29,26 @@ function App() {
     setPosts(posts.filter((el) => el.id !== post.id));
   };
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    console.log(sort);
+  };
+
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{ margin: '15px 0' }} />
+
+      <MySelect
+        value={selectedSort}
+        onChange={sortPosts}
+        defaultValue="Sorting by"
+        options={[
+          { value: 'title', name: 'By Title' },
+          { value: 'body', name: 'By Content' },
+        ]}
+      />
+
       {posts.length !== 0 ? (
         <PostList remove={removePost} posts={posts} title="Posts List" />
       ) : (
